05f4af39b1aa592612c39af6da25e72c93cee8f5
aloshai <aloshai@outlook.com>
2023-04-10 16:42:46 +0300

    chore

--- src/components/layout/aside.component.tsx
+++ src/components/layout/aside.component.tsx
@@ -54,6 +54,13 @@ export function Aside() {
         >
           <i className="ri-gift-line text-2xl"></i>
         </Link>
+        <Link
+          href="/top"
+          active-class="!text-violet-500"
+          className="flex h-12 w-12 items-center justify-center rounded-full hover:bg-neutral-800"
+        >
+          <i className="ri-arrow-up-double-line text-2xl"></i>
+        </Link>
         <button
           onClick={handleModal}
           className="flex h-12 w-12 items-center justify-center rounded-full hover:bg-neutral-800"
