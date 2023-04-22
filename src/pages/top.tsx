e88172bbffe9eb855685056330b5dfb49764d89a
aloshai <aloshai@outlook.com>
2023-04-22 21:34:14 +0300

    .

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -3,8 +3,9 @@ import { DefaultLayout } from '@/layouts/default.layout';
 import { useEffect, useRef, useState } from 'react';
 import { Transition } from '@headlessui/react';
 import classNames from 'classnames';
+import Image from 'next/image';
 
-export function Card({ className }: { className?: string }) {
+export function Card({ className, data }: { className?: string; data: any }) {
   return (
     <div className={`flex flex-col ${className}`}>
       <div className="mb-4 flex flex-col items-center justify-center">
@@ -13,20 +14,23 @@ export function Card({ className }: { className?: string }) {
             <div className="h-44 w-44 rounded-full bg-yellow-500/30 blur-[100px]"></div>
           </div>
           <div className="z-20">
-            <img
+            <Image
+              width={512}
+              height={512}
+              alt="card"
               className="!z-20 h-28 w-28 rounded-3xl backdrop-blur-3xl"
               src="/art/1.png"
             />
           </div>
         </div>
-        <h4 className="mt-4 text-xl font-medium">teeeest</h4>
+        <h4 className="mt-4 text-xl font-medium">{data.name}</h4>
       </div>
       <div className="top-path h-6 w-72 bg-gradient-to-b from-white/5 to-neutral-950/10"></div>
       <div className="flex h-44 w-72 flex-col items-center bg-gradient-to-b from-white/5 via-neutral-950/50 to-neutral-950/0 px-4 py-2 backdrop-blur-2xl">
         <div className="-mt-6 flex h-10 w-10 items-center justify-center rounded bg-white/80">
           <i className="ri-award-fill text-lg text-black/50"></i>
         </div>
-        <h4 className="mt-3 text-sm">33333</h4>
+        <h4 className="mt-3 text-sm">{data.point}</h4>
         <div className="mt-5 h-0.5 w-full bg-neutral-900"></div>
 
         <div className="mt-4">
@@ -43,10 +47,10 @@ export function Card({ className }: { className?: string }) {
 
 function UsersSection() {
   const [loaded, setLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     setLoaded(true);
-
   }, []);
 
   if (!loaded) {
@@ -56,20 +60,9 @@ function UsersSection() {
   return (
     <div>
       <div className="flex gap-8">
-        <Card className="mt-12"></Card>
-        <Card></Card>
-        <Card className="mt-12"></Card>
-      </div>
-
-      <div className="mt-12 max-w-2xl rounded-3xl bg-neutral-950/40 px-6 py-4 backdrop-blur-3xl">
-        You earned{' '}
-        <span>
-          <i className="ri-vip-diamond-fill text-lg text-blue-400"></i>{' '}
-          <span>
-            <b>50</b>
-          </span>
-        </span>{' '}
-        today and are ranked - out of <b>13.000 users</b>
+        {users[1] && <Card data={users[1]} className="mt-12"></Card>}
+        {users[0] && <Card data={users[0]}></Card>}
+        {users[2] && <Card data={users[2]} className="mt-12"></Card>}
       </div>
 
       <div className="mt-24 w-full max-w-screen-xl">
@@ -82,7 +75,7 @@ function UsersSection() {
           </div>
 
           <ul className="flex w-full flex-col gap-y-4">
-            {new Array(30).fill(0).map((item, i) => (
+            {users.map((user, i) => (
               <li
                 key={i}
                 className="grid grid-cols-4 rounded-xl bg-neutral-900/40 px-6 py-4 backdrop-blur-3xl"
@@ -92,10 +85,10 @@ function UsersSection() {
                   <span className="text-sm">{i + 1}</span>
                 </div>
                 <div className="col-span-1 flex items-center gap-1">
-                  {item.nickName}
+                  {user.nickName}
                 </div>
                 <div className="col-span-1 flex items-center gap-1">
-                  {item.point}
+                  {user.point}
                 </div>
                 <div className="col-span-1 flex items-center gap-1">
                   <div className="ml-auto flex gap-1 px-2 py-1">
@@ -114,6 +107,7 @@ function UsersSection() {
 
 function ClansSection() {
   const [loaded, setLoaded] = useState(false);
+  const [clans, setClans] = useState([]);
 
   useEffect(() => {
     setLoaded(true);
@@ -126,57 +120,9 @@ function ClansSection() {
   return (
     <div>
       <div className="flex gap-8">
-        <Card className="mt-12"></Card>
-        <Card></Card>
-        <Card className="mt-12"></Card>
-      </div>
-
-      <div className="mt-12 max-w-2xl rounded-3xl bg-neutral-950/40 px-6 py-4 backdrop-blur-3xl">
-        You earned{' '}
-        <span>
-          <i className="ri-vip-diamond-fill text-lg text-blue-400"></i>{' '}
-          <span>
-            <b>50</b>
-          </span>
-        </span>{' '}
-        today and are ranked - out of <b>13.000 users</b>
-      </div>
-
-      <div className="mt-24 w-full max-w-screen-xl">
-        <div className="flex flex-col">
-          <div className="mb-4 grid grid-cols-4 px-6 py-4">
-            <div className="col-span-1">Place</div>
-            <div className="col-span-1">Username</div>
-            <div className="col-span-1">Points</div>
-            <div className="col-span-1 text-right">Prize</div>
-          </div>
-
-          <ul className="flex w-full flex-col gap-y-4">
-            {new Array(30).fill(0).map((item, i) => (
-              <li
-                key={i}
-                className="grid grid-cols-4 rounded-xl bg-neutral-900/40 px-6 py-4 backdrop-blur-3xl"
-              >
-                <div className="col-span-1 flex items-center gap-1">
-                  <i className="ri-arrow-up-double-fill text-2xl"></i>
-                  <span className="text-sm">{i + 1}</span>
-                </div>
-                <div className="col-span-1 flex items-center gap-1">
-                  {item.nickName}
-                </div>
-                <div className="col-span-1 flex items-center gap-1">
-                  {item.point}
-                </div>
-                <div className="col-span-1 flex items-center gap-1">
-                  <div className="ml-auto flex gap-1 px-2 py-1">
-                    <i className="ri-vip-diamond-fill text-lg text-blue-400"></i>
-                    <span className="text-lg font-medium">5.000</span>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Card data={clans[1]} className="mt-12"></Card>
+        <Card data={clans[0]}></Card>
+        <Card data={clans[2]} className="mt-12"></Card>
       </div>
     </div>
   );
